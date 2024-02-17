6a6e0a7ca3fa423e4c4f7d45e4b4f4c487d5b4a6
Joselu <joseluissgz97@gmail.com>
2024-02-17 10:30:26 +0100

    prosConsDiscusser and prosConsDiscusserStream

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 import { OrthographyDto } from './dtos/orthography.dto';
-import { orthographyUseCase } from './uses-cases';
+import { ProsConsDiscusserDto } from './dtos/prosConsDiscusser.dto';
+import {
+  orthographyUseCase,
+  prosConsDicusserStreamUseCase,
+  prosConsDicusserUseCase,
+} from './uses-cases';
 
 @Injectable()
 export class GptService {
@@ -11,7 +16,19 @@ export class GptService {
 
   async orthographyCheck(body: OrthographyDto) {
     return await orthographyUseCase(this.opeAi, {
-      prompt: body.req,
+      prompt: body.prompt,
+    });
+  }
+
+  async prosConsDiscusser({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDicusserUseCase(this.opeAi, {
+      prompt,
+    });
+  }
+
+  async prosConsDiscusserStream({ prompt }: ProsConsDiscusserDto) {
+    return await prosConsDicusserStreamUseCase(this.opeAi, {
+      prompt,
     });
   }
 }
