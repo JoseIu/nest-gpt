baee639cb5d55760225583a6771b2cddcfb879a4
Joselu <joseluissgz97@gmail.com>
2024-02-17 10:32:27 +0100

    Pront to prosConsDicusserUseCase

---
+++ src/gpt/uses-cases/prosConsDicusser-use-case.ts
@@ -0,0 +1,34 @@
+import OpenAI from 'openai';
+
+interface Options {
+  prompt: string;
+}
+
+export const prosConsDicusserUseCase = async (
+  openAi: OpenAI,
+  options: Options,
+) => {
+  const { prompt } = options;
+
+  const completion = await openAi.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `Se te dará una pregunta y tu tarea es dar una respuesta con pros y contras,
+        la respuesta debe de ser en formato markdown,
+        los pros y contras deben de estar en una lista,
+        `,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.8,
+    top_p: 1,
+    model: 'gpt-3.5-turbo',
+  });
+
+  //   const jsonRes = JSON.parse(completion.choices[0].message.content);
+  return completion.choices[0].message.content;
+};
