a699f2caeed3187b1a259b304f3e7298b24c8929
Joselu <joseluissgz97@gmail.com>
2024-02-17 10:30:12 +0100

    path  pros-cons-discusser and pros-cons-discusser-stream

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { OrthographyDto } from './dtos/orthography.dto';
+import { ProsConsDiscusserDto } from './dtos/prosConsDiscusser.dto';
 import { GptService } from './gpt.service';
 
 @Controller('gpt')
@@ -9,5 +11,29 @@ export class GptController {
   @Post('orthography')
   orthographyCheck(@Body() body: OrthographyDto) {
     return this.gptService.orthographyCheck(body);
+  }
+
+  @Post('pros-cons-discusser')
+  prosConsDiscusser(@Body() body: ProsConsDiscusserDto) {
+    return this.gptService.prosConsDiscusser(body);
+  }
+
+  @Post('pros-cons-discusser-stream')
+  async prosConsDicusserStream(
+    @Body() Body: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+    const stream = await this.gptService.prosConsDiscusserStream(Body);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(HttpStatus.OK);
+
+    for await (const chunk of stream) {
+      const piece = chunk.choices[0].delta.content || '';
+      // console.log(piece);
+      res.write(piece);
+    }
+
+    res.end();
   }
 }
