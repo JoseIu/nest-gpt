fb2efc60372ad82aa1a699751dee2ea1dac604a3
Joselu <joseluissgz97@gmail.com>
2024-02-18 21:26:21 +0100

    translateUseCase

---
+++ src/gpt/uses-cases/translate-use-case.ts
@@ -0,0 +1,17 @@
+import OpenAI from 'openai';
+
+export const translateUseCase = async (opeAi: OpenAI, { prompt, lang }) => {
+  const completion = await opeAi.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+
+    messages: [
+      {
+        role: 'system',
+        content: `Se te dará un texto o documento y tu tarea es traducirlo al idioma ${lang}:${prompt}`,
+      },
+    ],
+    temperature: 0.2,
+  });
+
+  return completion.choices[0].message.content;
+};
