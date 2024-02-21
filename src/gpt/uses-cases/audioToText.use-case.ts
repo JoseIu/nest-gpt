a5188cc09988e65ea6587dbbb4e4fbb04f78512a
Joselu <joseluissgz97@gmail.com>
2024-02-21 19:04:34 +0100

    audioToTextUseCase

--- src/gpt/uses-cases/audioToText.use-case.ts
+++ src/gpt/uses-cases/audioToText.use-case.ts
@@ -1,3 +1,21 @@
-export const audioToTextUseCase = async (prompt, audio) => {
-  console.log({ prompt, audio });
+import * as fs from 'fs';
+import OpenAI from 'openai';
+
+interface Options {
+  prompt: string;
+  audioFIle: Express.Multer.File;
+}
+
+export const audioToTextUseCase = async (openAi: OpenAI, options: Options) => {
+  const { prompt, audioFIle } = options;
+
+  const response = await openAi.audio.transcriptions.create({
+    model: 'whisper-1',
+    file: fs.createReadStream(audioFIle.path),
+    prompt: prompt, //Mismo idioma que el audio
+    language: 'es',
+    response_format: 'verbose_json',
+  });
+
+  return response;
 };
