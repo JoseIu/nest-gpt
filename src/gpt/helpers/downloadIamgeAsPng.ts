bb70690460abd197e921ebf0fb7bb84f9c006b11
Joselu <joseluissgz97@gmail.com>
2024-02-24 11:27:57 +0100

    downloadBase64ImageAsPng and fullPath

--- src/gpt/helpers/downloadIamgeAsPng.ts
+++ src/gpt/helpers/downloadIamgeAsPng.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as sharp from 'sharp';
 import { v4 as uuidv4 } from 'uuid';
 
-export const downLoadImageAsPng = async (url: string) => {
+export const downLoadImageAsPng = async (url: string, fullPath: boolean = false) => {
   const response = await fetch(url);
 
   if (!response.ok) throw new InternalServerErrorException('Error al obtener la imagen');
@@ -18,9 +18,25 @@ export const downLoadImageAsPng = async (url: string) => {
 
   const completePath = path.join(folderPath, imageNamePng);
 
-  //   fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer);
-
   await sharp(buffer).png().ensureAlpha().toFile(completePath);
 
-  return completePath;
+  return fullPath ? completePath : imageNamePng;
+};
+
+export const downloadBase64ImageAsPng = async (base64Image: string, fullPath: boolean = false) => {
+  // Remover encabezado
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imageNamePng = `${uuidv4()}-64.png`;
+
+  const completePath = path.join(folderPath, imageNamePng);
+
+  // Transformar a RGBA, png // Así lo espera OpenAI
+  await sharp(imageBuffer).png().ensureAlpha().toFile(completePath);
+
+  return fullPath ? completePath : imageNamePng;
 };
