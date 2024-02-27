2e317ccb6ddfeeb62b36e29501035a2664f6f6f8
Joselu <joseluissgz97@gmail.com>
2024-02-27 18:31:04 +0100

    checkCompleteStatusUseCase

---
+++ src/sam-assistant/uses-cases/checkCompleteStatus.use-case.ts
@@ -0,0 +1,21 @@
+import OpenAI from 'openai';
+
+interface Options {
+  threadId: string;
+  runId: string;
+}
+
+export const checkCompleteStatusUseCase = async (opeAi: OpenAI, options: Options) => {
+  const { threadId, runId } = options;
+
+  const runStatus = await opeAi.beta.threads.runs.retrieve(threadId, runId);
+
+  if (runStatus.status === 'completed') {
+    return runStatus;
+  }
+
+  //esperamos 1seg
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return await checkCompleteStatusUseCase(opeAi, options);
+};
