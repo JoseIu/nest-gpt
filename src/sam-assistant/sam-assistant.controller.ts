c13070f4a355c2dda13d41b692ae8550e73a6406
Joselu <joseluissgz97@gmail.com>
2024-02-27 18:30:11 +0100

    post create-thread and user-question

---
+++ src/sam-assistant/sam-assistant.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserQuestionDto } from './dtos/userQuestion.dto';
+import { SamAssistantService } from './sam-assistant.service';
+
+@Controller('gpt')
+export class SamAssistantController {
+  constructor(private readonly samAssistantService: SamAssistantService) {}
+
+  @Post('create-thread')
+  async creatThread() {
+    return this.samAssistantService.creatThread();
+  }
+
+  @Post('user-question')
+  async userCuestion(@Body() body: UserQuestionDto) {
+    return this.samAssistantService.createMessage(body);
+  }
+}
